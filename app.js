0f666549960ee6b78cf72b12778be788cf50ef4a
janek26 <janek.rahrt@me.com>
2015-11-24 14:41:16 +0100

    u

--- app.js
+++ app.js
@@ -1,17 +1,28 @@
 var unirest = require('unirest');
 var hookUrl = "http://api.address-validator.net/api/verify";
 
-module.export = function(apikey) {
-  if (isValidAPIKey(apikey)) {
-    console.log(apikey);
+module.exports = function(apikey) {
+  if (!isValidAPIKey(apikey)) {
+    throw new Error("No valid API Key!");
   } else {
-    console.log("No valid APIKey!");
+    AVapikey = apikey;
+  }
+  return function(address){
+    console.log(address);
+    this.getAPIKey = function(){
+      return(AVapikey);
+    }
+    return {
+      getAPIKey: function(){
+        return(apikey);
+      }
+    }
   }
 }
 
 function isValidAPIKey(apikey) {
   if (apikey.substring(0, 3) == "iv-") {
-    var regex = "/^[a-f0-9]{32}$/g";
+    var regex = /^[a-f0-9]{32}$/g;
     return regex.test(apikey.substring(3));
   } else {
     return false;
--- test.js
+++ test.js
@@ -1,1 +1,3 @@
-var addressValidator = require('./');
+var addressValidator = require('./')("iv-ae8974a7adf18cea9dc2e055739fa6b6");
+console.log(addressValidator.getAPIKey);
+addressValidator("Mühle");
