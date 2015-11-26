dcb6582b68785eaa5fb5c27a82d3b4942e6d88d7
janek26 <janek.rahrt@me.com>
2015-11-26 11:42:50 +0100

    finally :)

--- app.js
+++ app.js
@@ -1,19 +1,49 @@
 var unirest = require('unirest');
-var hookUrl = "http://api.address-validator.net/api/verify";
+var hookUrl = "https://api.address-validator.net/api/verify";
 
 module.exports = function(apikey) {
   if (!isValidAPIKey(apikey)) {
     throw new Error("No valid API Key!");
   }
-  return function(address){
-    return {
-      getAPIKey: function() {
-        return apikey;
-      },
-      getAddress: function() {
-        return address;
-      }
-    }
+  return function(address, cb){
+    var url = hookUrl +"?"+
+    "StreetAddress="+address.StreetAddress+
+    "&City="+address.City+
+    "&PostalCode="+address.PostalCode+
+    "&CountryCode="+address.CountryCode+
+    "&APIKey="+apikey;
+    unirest.get(url).end(function (response) {
+      var resp = response.body;
+      var formattedaddress = resp.formattedaddress;
+      var status = resp.status;
+      delete resp.status;
+      delete resp.formattedaddress;
+      cb({
+        getAPIKey: function() {
+          return apikey;
+        },
+        getAddress: function() {
+          return address;
+        },
+        isValid: function() {
+          return status;
+        },
+        serialized: function() {
+          if (this.isValid == "INVALID") {
+            return false;
+          } else {
+            return resp;
+          }
+        },
+        serializedString: function() {
+          if (this.isValid == "INVALID") {
+            return false;
+          } else {
+            return formattedaddress;
+          }
+        }
+      });
+    });
   }
 }
 
--- test.js
+++ test.js
@@ -1,3 +1,9 @@
 var addressValidator = require('./')("iv-ae8974a7adf18cea9dc2e055739fa6b6");
-var address1 = addressValidator("Mühle");
-console.log(address1.getAPIKey(), address1.getAddress());
+var address1 = addressValidator({
+    StreetAddress : "Mühlenstraße 26",
+    City          : "Ocholt",
+    PostalCode    : "26655",
+    CountryCode   : "de"
+  }, function(response) {
+    console.log(response.getAPIKey(), response.getAddress(), response.serialized(), response.isValid(), response.serializedString());
+  });
