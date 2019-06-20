afd9c47f94e687579cefda8a1bc6ff70042b001e
Janek Rahrt <janek.rahrt@me.com>
2019-06-20 18:52:46 +0200

    v2.0.1

--- src/__tests__/isValidAPIKey.test.ts
+++ src/__tests__/isValidAPIKey.test.ts
@@ -8,4 +8,5 @@ test('isValidAPIKey', () => {
   expect(isValidAPIKey('av-df799342fa9405da27916bcc96e46a5e')).toBe(true)
   expect(isValidAPIKey('av-cd180873104fe2eb1edb20297b8d024e')).toBe(true)
   expect(isValidAPIKey('av-416b1aa14fd1d8bed9b53979bbb64293')).toBe(true)
+  expect(isValidAPIKey('df-416b1aa14fd1d8bed9b53979bbb64293')).toBe(true)
 })
