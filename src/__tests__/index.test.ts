a839b8b4442605a16746cb449695c740f9962e2f
thomas <thomas@byteplant.com>
2022-10-17 13:02:34 +0200

    2.1.0

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -14,7 +14,6 @@ const ADDRESSV = {
   PostalCode: '26655',
   StreetAddress: 'Mühlenstraße 26',
 }
-const ADDRESS_ID = { ID: 'WFR7sEMtAp2323YTbCGdecjpUk-ax35tEA' }
 const ADDRESSAC = {
   CountryCode: 'de',
   Query: 'Mühlenstraße 26, westerstede',
@@ -39,15 +38,12 @@ test('Use instance to check address', async () => {
   const response = await call
 
   expect(response).toMatchObject({
-    addressline1: 'Mühlenstr. 26',
-    addressline3: '26655 Westerstede',
-    city: 'Westerstede',
+    city: 'Westerstede,Ocholt',
     country: 'DE',
-    formattedaddress: 'Mühlenstr. 26,26655 Westerstede',
+    formattedaddress: 'Ocholt,Mühlenstraße 26,26655 Westerstede',
     postalcode: '26655',
-    state: 'Niedersachsen',
-    status: 'SUSPECT',
-    street: 'Mühlenstr.',
+    status: 'VALID',
+    street: 'Mühlenstraße',
     streetnumber: '26',
   })
 })
@@ -63,36 +59,15 @@ test('Use instance to autocomplete address', async () => {
   expect(response).toMatchObject({
     results: [
       {
-        "description": "Mühlenstraße 26, Westerstede, Germany",
-        "id": "VmtWb45sfH0Wk84xf_s08pjEPMD5",
+        "description": "Große Mühlenstraße 26,26655 Westerstede",
+        "id": "TVFxWYhd2KUY5Ys4KocqDD28F-tto-k_KF8", 
       },
       {
-        "description": "Große Mühlenstraße 26, Westerstede, Germany",
-        "id": "IUdyb8OfZSBNw7xobGVuc3RyYcOfZSAyNiwgV2VzdGVyc3RlZGUsIEdlcm1hbnk",
+	"description": "Mühlenstraße 26,26655 Westerstede",
+	"id": "TVBxWYltbFeTs8x4C2g-mn_V4Xdto-k_OB4",
       },
     ],
     status: 'UNVERIFIED',
   })
 })
 
-test('Use instance to retrieve address', async () => {
-  const { retrieve: instance } = AddressValidator(WORKING_APIKEY)
-  const call = instance(ADDRESS_ID)
-
-  expect(call).toBeInstanceOf(Promise)
-
-  const response = await call
-
-  expect(response).toMatchObject({
-    addressline1: 'Mühlenstraße 26',
-    addressline3: '26655 Westerstede',
-    city: 'Westerstede',
-    country: 'DE',
-    formattedaddress: 'Mühlenstraße 26,26655 Westerstede',
-    postalcode: '26655',
-    state: 'Niedersachsen',
-    status: 'UNVERIFIED',
-    street: 'Mühlenstraße',
-    streetnumber: '26',
-  })
-})
